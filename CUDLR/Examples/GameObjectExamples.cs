6cb0d2fc62fc85b2f6c4602edccb685f3fb77ead
Robert D. Blanchet Jr <xgalaxy@gmail.com>
2013-10-22 19:17:55 -0700

    Improved object list and object print examples.

--- CUDLR/Examples/GameObjectExamples.cs
+++ CUDLR/Examples/GameObjectExamples.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using System.Collections.Generic;
 using System.Reflection;
 using System.Net;
+using System;
 
 /**
  * Example console commands for getting information about GameObjects
@@ -10,28 +11,37 @@ public static class GameObjectCommands {
 
   [CUDLR.Command("object list", "lists all the game objects in the scene")]
   public static void ListGameObjects() {
-    UnityEngine.Object[] objects = UnityEngine.Object.FindObjectsOfType(typeof(GameObject));
-    foreach (UnityEngine.Object obj in objects) {
-      CUDLR.Console.Log(obj.name);
+    Action<Transform, int> deepList = (Transform xform, int indentLevel) => {
+      string listing = "- " + xform.name;
+      CUDLR.Console.Log(listing.PadLeft(listing.Length + indentLevel * 2));
+
+      int childCount = xform.childCount;
+      for (int child = 0; child < childCount; child++)
+        deepList(xform.GetChild(child), indentLevel + 1);
+    };
+
+    foreach (Transform xform in GameObject.FindObjectsOfType(typeof(Transform))) {
+      if (xform.parent == null)
+        deepList(xform, 0);
     }
   }
 
   [CUDLR.Command("object print", "lists properties of the object")]
   public static void PrintGameObject(string[] args) {
     if (args.Length < 1) {
-      CUDLR.Console.Log( "expected : object print <Object Name>" );
+      CUDLR.Console.Log("expected: object print <Object Name>");
       return;
     }
 
     GameObject obj = GameObject.Find( args[0] );
     if (obj == null) {
-      CUDLR.Console.Log("GameObject not found : "+args[0]);
+      CUDLR.Console.Log("GameObject not found: "+args[0]);
     } else {
-      CUDLR.Console.Log("Game Object : "+obj.name);
+      CUDLR.Console.Log("Game Object: "+obj.name);
       foreach (Component component in obj.GetComponents(typeof(Component))) {
-       CUDLR.Console.Log("  Component : "+component.GetType());
+       CUDLR.Console.Log("   + Component: "+component.GetType());
         foreach (FieldInfo f in component.GetType().GetFields()) {
-          CUDLR.Console.Log("    "+f.Name+" : "+f.GetValue(component));
+          CUDLR.Console.Log("     - "+f.Name+":  "+f.GetValue(component));
         }
       }
     }
@@ -47,7 +57,7 @@ public static class GameObjectCommands {
 // FIXME need main thread support
 
 public static class GameObjectRoutes {
-  
+
   [CUDLR.Route("^/object/list.json$")]
   public static bool ListGameObjects(HttpListenerContext context) {
     string json = "[";
